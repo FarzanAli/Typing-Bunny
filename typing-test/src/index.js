f56b543053c7f0a016aa0e1b5c4d1b72ed7b6167
Farzan <farzanalif42@gmail.com>
2021-06-14 13:03:54 +0500

    Update index.js

--- typing-test/src/index.js
+++ typing-test/src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Stats from './components/stats.js';
