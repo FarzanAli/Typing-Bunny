2082ff5fdda80f065bf21c989e63fd2a3404102e
Farzan <farzanalif42@gmail.com>
2021-06-29 16:53:23 +0500

    update
    useSound implemented. toggleLight refactored.

--- typing-test/src/components/toggleLight.js
+++ typing-test/src/components/toggleLight.js
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaMoon } from 'react-icons/fa';
-
-export default class ToggleLight extends Component{
+import useSound from 'use-sound';
+import LightSwitchOn from './audio/light-switch/light-switch-on.mp3';
+import LightSwitchOff from './audio/light-switch/light-switch-off.mp3';
+let ToggleLight = () => {
     
-    componentDidMount(){
-        document.getElementsByClassName("toggleLight-container").item(0).addEventListener('mousedown', () => {
-            if(document.documentElement.getAttribute('theme') === "light"){
-                trans();
-                document.documentElement.setAttribute('theme', 'dark');
-            }
-            else if(document.documentElement.getAttribute('theme') === "dark"){
-                trans();
-                document.documentElement.setAttribute('theme', 'light');
-            }
-        });
+    const [isOn, setIsOn] = useState(true);
+    const [playOn] = useSound(LightSwitchOn);
+    const [playOff] = useSound(LightSwitchOff);
+    useEffect(() => {
+        isOn ? document.documentElement.setAttribute('theme', 'light') : document.documentElement.setAttribute('theme', 'dark');
+        isOn ? playOn() : playOff();
+        document.documentElement.classList.add('transition');
+        window.setTimeout(() => {
+            document.documentElement.classList.remove('transition');
+        }, 1000); 
+    }, [isOn]);
 
-        let trans = () => {
-            document.documentElement.classList.add('transition');
-            window.setTimeout(() => {
-                document.documentElement.classList.remove('transition')
-            }, 1000)
-        }
-    }
-
-    render(){
-        return(
-            <button type="button" className="toggleLight-container">
-                <FaMoon style={{width: `100%`, height: `100%`}}/>
-            </button>
-        );
-    }
+    return(
+        <button type="button" className="toggleLight-container" onClick={() => setIsOn(!isOn)}>
+            <FaMoon style={{width: `100%`, height: `100%`}}/>
+        </button>
+    );
 }
+
+export default ToggleLight;
